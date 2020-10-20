d224e474fddabe59aa88f9ed5cf2a707dccc8404
kaneLittle2020 <5littk17@solent.ac.uk>
2020-10-20 11:26:56 +0000

    fin

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 def movements():
-  path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
+  path = ["Move Forward",  "Move Backward",  "Move Left", "Move Right"]
   return path
 
 def menu():
   print ("please select a direction:")
-  route = movements()
-  for index in range (len(route)):
-    print("{} : {}". format(index, route [index]))
+  path = movements()
+
+  for index in range (len(path)):
+    print("{} : {}". format(index, path [index]))
 
 def run():
-   print("Moving...")
-   path = movements()
-   print ("{} for {} steps".format(path[0], path[1]))
-   print ("{} for {} steps".format(path[2], path[3]))
-   print ("{} for {} steps".format(path[4], path[5]))
-   print ("{} for {} steps".format(path[6], path[7]))
+   menu()
 
 
 
 
-menu()
+
 run()
--- simple_list.py
+++ simple_list.py
@@ -21,3 +21,65 @@ def run():
 
   
 run()
+---------------------------
+def movements():
+  path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
+  return path
+
+def menu():
+  print ("please select a direction:")
+  route = movements()
+  for index in range (len(route)):
+    print("{} : {}". format(index, route [index]))
+
+def run():
+   print("Moving...")
+   path = movements()
+   print ("{} for {} steps".format(path[0], path[1]))
+   print ("{} for {} steps".format(path[2], path[3]))
+   print ("{} for {} steps".format(path[4], path[5]))
+   print ("{} for {} steps".format(path[6], path[7]))
+
+
+
+
+menu()
+run()
+-------------------------------
+def movements():
+  path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
+  return path
+
+
+
+def run():
+   print("Moving...")
+   path = movements()
+   for index in range(0, len(path),2):
+    print("{} for {} steps".format(path [index] , path [index + 1 ]))
+
+
+
+
+
+run()
+-----------------------------------
+def movements():
+  path = ["Move Forward",  "Move Backward",  "Move Left", "Move Right"]
+  return path
+
+def menu():
+  print ("please select a direction:")
+  path = movements()
+
+  for index in range (len(path)):
+    print("{} : {}". format(index, path [index]))
+
+def run():
+   menu()
+
+
+
+
+
+run()
